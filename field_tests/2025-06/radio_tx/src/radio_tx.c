e4aa652a41540147955cbacccdce6b9f4e4b32bd
amelia <amelia@ag2v.com>
2025-06-05 18:35:15 -0400

    made printing time work(?)

--- field_tests/2025-06/radio_tx/src/radio_tx.c
+++ field_tests/2025-06/radio_tx/src/radio_tx.c
@@ -30,27 +30,67 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 #include "tusb.h"
 
 #include "whale.h"
 #include "pcf8523_rp2040.h"
 
-#define RTC_SDA
-#define RTC_SCL
+#define I2C_INST (i2c0)
+#define PIN_SCL  (5)
+#define PIN_SDA  (4)
+#define PIN_IRQ  (2)
+
 
 void main() {
 	stdio_init_all();
 	//while (!tud_cdc_connected()) { sleep_ms(100); };
+
+	//init i2c for rtc
+	i2c_init(I2C_INST, 500 * 1000);
+	gpio_set_function(PIN_SCL, GPIO_FUNC_I2C);
+	gpio_set_function(PIN_SDA, GPIO_FUNC_I2C);
+	gpio_pull_up(PIN_SCL);
+	gpio_pull_up(PIN_SDA);
+	gpio_pull_up(PIN_IRQ);
+	uint index = I2C_NUM(I2C_INST);
+
+	//rtc init
+	if(!pcf8523_software_reset_initiate(index)) goto ERROR_LOOP;
+	bool timecheck;
+	pcf8523_time_circuit_is_running(index, &timecheck);
+	if(!timecheck) {
+		printf("time circuit not running!\n");
+		goto ERROR_LOOP;
+	}
+
+	for(;;) {
+	uint8_t seconds;
+	pcf8523_seconds_get(index, &seconds);	
+	uint8_t minutes;
+	pcf8523_minutes_get(index, &minutes);	
+	uint8_t hours;
+	pcf8523_hours_get(index, &hours);
+	uint8_t years;
+	pcf8523_years_get(index, &years);
+	uint8_t months;
+	pcf8523_months_get(index, &months);
+	uint8_t days;
+	pcf8523_days_get(index, &days);
 	
-	struct pcf8523_time_date_s rtc_datetime;
-	pcf8523_time_date_get_all(,&rtc_datetime);
 
+	
+	//for serial monitor purposes
 	sleep_ms(2000);
-	printf("meow");
+	printf("%02u-%02u-%02uT%02u:%02u:%02u\n", 
+			years, months, days,
+			hours, minutes, seconds);
+	}
 
-
+	//radio init
 	int rval = whale_init(W_RADIO_MODULE);
 	if (rval != WHALE_OK)
 		goto ERROR_LOOP;
@@ -63,7 +103,7 @@ void main() {
 		payload[i] = i;
 
 	uint8_t success[] = "tx success";
-uint8_t failure[] = "tx failure";
+	uint8_t failure[] = "tx failure";
 
 	w_radio_dbm_set(20);
 
@@ -82,7 +122,7 @@ uint8_t failure[] = "tx failure";
 	// Loop forever with error
 ERROR_LOOP:
 	for (;;) {
-		printf("Radio module failed to initialize: ");
+		printf("Error!");
 		printf("%u\n", w_module_state_query(W_RADIO_MODULE));
 		sleep_ms(3000);
 	}
